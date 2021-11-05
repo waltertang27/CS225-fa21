705b9b05c33b713d614a6fef0f3ef8a1e70dbb94
Walter Tang <waltert3@illinois.edu>
2021-11-04 20:55:43 -0500

    lab_heaps

--- lab_heaps/heap.cpp
+++ lab_heaps/heap.cpp
@@ -8,35 +8,35 @@ template <class T, class Compare>
 size_t heap<T, Compare>::root() const
 {
     // @TODO Update to return the index you are choosing to be your root.
-    return 0;
+    return 1;
 }
 
 template <class T, class Compare>
 size_t heap<T, Compare>::leftChild(size_t currentIdx) const
 {
     // @TODO Update to return the index of the left child.
-    return 0;
+    return currentIdx * 2;
 }
 
 template <class T, class Compare>
 size_t heap<T, Compare>::rightChild(size_t currentIdx) const
 {
     // @TODO Update to return the index of the right child.
-    return 0;
+    return currentIdx * 2 + 1;
 }
 
 template <class T, class Compare>
 size_t heap<T, Compare>::parent(size_t currentIdx) const
 {
     // @TODO Update to return the index of the parent.
-    return 0;
+    return 0.5 * currentIdx;
 }
 
 template <class T, class Compare>
 bool heap<T, Compare>::hasAChild(size_t currentIdx) const
 {
     // @TODO Update to return whether the given node has a child
-    return false;
+    return _elems.size() > leftChild(currentIdx);
 }
 
 template <class T, class Compare>
@@ -44,13 +44,28 @@ size_t heap<T, Compare>::maxPriorityChild(size_t currentIdx) const
 {
     // @TODO Update to return the index of the child with highest priority
     ///   as defined by higherPriority()
-    return 0;
+
+    if(rightChild(currentIdx) >= _elems.size()) {
+        return leftChild(currentIdx);
+    }
+    if(higherPriority(_elems[leftChild(currentIdx)], _elems[rightChild(currentIdx)])) {
+        return leftChild(currentIdx);
+    }
+    return rightChild(currentIdx);
 }
 
 template <class T, class Compare>
 void heap<T, Compare>::heapifyDown(size_t currentIdx)
 {
     // @TODO Implement the heapifyDown algorithm.
+    if(leftChild(currentIdx) >= _elems.size()) {
+        return;
+    }
+    size_t childIndex = maxPriorityChild(currentIdx);
+    if(higherPriority(_elems[childIndex], _elems[currentIdx])) {
+        std::swap(_elems[childIdx], _elems[currentIdx]);
+        heapifyDown(childIdx);
+    }
 }
 
 template <class T, class Compare>
@@ -70,32 +85,49 @@ heap<T, Compare>::heap()
 {
     // @TODO Depending on your implementation, this function may or may
     ///   not need modifying
+    _elems.push_back(T());
 }
 
 template <class T, class Compare>
 heap<T, Compare>::heap(const std::vector<T>& elems) 
 {
     // @TODO Construct a heap using the buildHeap algorithm
+    //_elems = {T()};
+    for(auto i : elems) {
+        _elems.push_back(i);
+    }
+    for(size_t j = _elems.size() - 1; i > 0; i--) {
+        heapifyDown(j);
+    }
 }
 
 template <class T, class Compare>
 T heap<T, Compare>::pop()
 {
     // @TODO Remove, and return, the element with highest priority
-    return T();
+    if(_elems.size() == 1) {
+        return T();
+    }
+    T temp = _elems.at(1);
+    std::swap(_elems.at(1), _elems.back());
+    _elems.pop_back();
+    heapifyDown(1);
+    return temp;
 }
 
 template <class T, class Compare>
 T heap<T, Compare>::peek() const
 {
     // @TODO Return, but do not remove, the element with highest priority
-    return T();
+    return _elems.at(1);
 }
 
 template <class T, class Compare>
 void heap<T, Compare>::push(const T& elem)
 {
     // @TODO Add elem to the heap
+    _elems.push_back(elem);
+    heapifyUp(_elems.size() - 1);
 }
 
 template <class T, class Compare>
@@ -103,6 +135,14 @@ void heap<T, Compare>::updateElem(const size_t & idx, const T& elem)
 {
     // @TODO In-place updates the value stored in the heap array at idx
     // Corrects the heap to remain as a valid heap even after update
+    T temp = _elems[idx];
+    _elems[idx] = elem;
+    if(higherPriority(elem, temp) {
+        heapifyUp(idx);
+    }
+    else {
+        heapifyDown(idx);
+    }
 }
 
 
@@ -110,6 +150,9 @@ template <class T, class Compare>
 bool heap<T, Compare>::empty() const
 {
     // @TODO Determine if the heap is empty
+    if(_elems.size() <= 1) {
+        return true;
+    }
     return true;
 }
 
